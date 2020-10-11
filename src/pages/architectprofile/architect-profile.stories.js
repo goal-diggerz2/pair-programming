de3ded3cc3a3783e685f065a468031697e5bf444
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-11 21:28:07 +0200

    Create architect-profile - general layout

---
+++ src/pages/architectprofile/architect-profile.stories.js
@@ -0,0 +1,6 @@
+import "./architect-profile.css";
+import architectprofile from "./architect-profile.html";
+
+export default { title: "Pages/ArchitectProfile" };
+
+export const ArchitectProfile = () => architectprofile;
